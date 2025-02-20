6113fe47ae21b1c7a70a927720fe4dfedf53d055
atka66 <atkavagyok@gmail.com>
2025-02-20 22:02:02 +0100

    some cleanup

--- wfc.py
+++ wfc.py
@@ -10,48 +10,49 @@ from importer import *
 TILE_DEFS = []
 
 MAP = []
-MAP_WIDTH = 60
-MAP_HEIGHT = 40
+MAP_WIDTH = 100
+MAP_HEIGHT = 50
 TILE_SIZE = 16
 
-def resetMap():
+def resetMap(border=True):
     now = datetime.now()
     MAP.clear()
     for _ in range(MAP_HEIGHT):
         mapRow = []
         for _ in range(MAP_WIDTH):
-            mapRow.append({"collapsed": '', "values": [i for i in range(len(TILE_DEFS))]})
+            mapRow.append({"collapsed": False, "values": [i for i in range(len(TILE_DEFS))]})
         MAP.append(mapRow)
     
-    collapseAt(random.randrange(MAP_WIDTH), random.randrange(MAP_HEIGHT), random.randrange(len(TILE_DEFS)))
+    if border:
+        for i in range(MAP_HEIGHT):
+            collapseAt(0, i, 0)
+            collapseAt(MAP_WIDTH - 1, i, 0)
+        for i in range(MAP_WIDTH):
+            collapseAt(i, 0, 0)
+            collapseAt(i, MAP_HEIGHT - 1, 0)
+    else:
+        collapseAt(random.randrange(MAP_WIDTH), random.randrange(MAP_HEIGHT), random.randrange(len(TILE_DEFS)))
 
     elapsedTime = datetime.now() - now
     #print(f"map has been reset - {elapsedTime}")
     return elapsedTime
 
+def removeForbidden(x, y, tileNeighbour, neighbourId):
+    if 0 <= y and y < MAP_HEIGHT and 0 <= x and x < MAP_WIDTH and not MAP[y][x]["collapsed"]:
+        expectedPattern = tileNeighbour[::-1]
+        forbiddenTiles = [idx for idx, e in enumerate(TILE_DEFS) if e["neighbours"][neighbourId] != expectedPattern]
+        MAP[y][x]["values"] = [e for e in MAP[y][x]["values"] if e not in forbiddenTiles]
+
 def collapseAt(x, y, value):
     MAP[y][x]["values"] = [value]
-    MAP[y][x]["collapsed"] = 'A'
+    MAP[y][x]["collapsed"] = True
 
-    expectedAbove = TILE_DEFS[value]["neighbours"][0][::-1]
-    if y - 1 >= 0 and not MAP[y - 1][x]["collapsed"]:
-        forbiddenAboveTiles = [idx for idx, e in enumerate(TILE_DEFS) if e["neighbours"][2] != expectedAbove]
-        MAP[y - 1][x]["values"] = [e for e in MAP[y - 1][x]["values"] if e not in forbiddenAboveTiles]
+    tileNeighbours = TILE_DEFS[value]["neighbours"]
 
-    expectedBelow = TILE_DEFS[value]["neighbours"][2][::-1]
-    if y + 1 < MAP_HEIGHT and not MAP[y + 1][x]["collapsed"]:
-        forbiddenBelowTiles = [idx for idx, e in enumerate(TILE_DEFS) if e["neighbours"][0] != expectedBelow]
-        MAP[y + 1][x]["values"] = [e for e in MAP[y + 1][x]["values"] if e not in forbiddenBelowTiles]
-    
-    expectedRight = TILE_DEFS[value]["neighbours"][1][::-1]
-    if x + 1 < MAP_WIDTH and not MAP[y][x + 1]["collapsed"]:
-        forbiddenRightTiles = [idx for idx, e in enumerate(TILE_DEFS) if e["neighbours"][3] != expectedRight]
-        MAP[y][x + 1]["values"] = [e for e in MAP[y][x + 1]["values"] if e not in forbiddenRightTiles]
-    
-    expectedLeft = TILE_DEFS[value]["neighbours"][3][::-1]
-    if x - 1 >= 0 and not MAP[y][x - 1]["collapsed"]:
-        forbiddenLeftTiles = [idx for idx, e in enumerate(TILE_DEFS) if e["neighbours"][1] != expectedLeft]
-        MAP[y][x - 1]["values"] = [e for e in MAP[y][x - 1]["values"] if e not in forbiddenLeftTiles]
+    removeForbidden(x, y - 1, tileNeighbours[0], 2)
+    removeForbidden(x, y + 1, tileNeighbours[2], 0)
+    removeForbidden(x + 1, y, tileNeighbours[1], 3)
+    removeForbidden(x - 1, y, tileNeighbours[3], 1)
 
 def findLeastValuesPos():
     leastValuesN = len(TILE_DEFS) + 1
@@ -75,11 +76,11 @@ def findLeastValuesPos():
 def isMapCollapsed():
     for i in range(MAP_HEIGHT):
         for j in range(MAP_WIDTH):
-            if MAP[i][j]["collapsed"] == '':
+            if not MAP[i][j]["collapsed"]:
                 return ''
     return 'A'
 
-def collapseMap(root, canvas, showInbetween):
+def collapseMap(root, canvas, showInbetween, border):
     now = datetime.now()
 
     while(not isMapCollapsed()):
@@ -91,10 +92,8 @@ def collapseMap(root, canvas, showInbetween):
         y = pos[1]
         values = MAP[y][x]["values"]
         if not values:
-            #print(f"WARNING - contradiction occurred at position ({x},{y})")
-            #MAP[y][x]["values"] = [-1]
-            #MAP[y][x]["collapsed"] = 'A'
-            resetMap()
+            print(f"WARNING - contradiction occurred at position ({x},{y})")
+            resetMap(border)
         else:
             collapseAt(x, y, random.choice(values))
 
@@ -106,7 +105,7 @@ def collapseMap(root, canvas, showInbetween):
     print(f"map has collapsed - {elapsedTime}")
     return elapsedTime
 
-def benchmarkMap(root, canvas, showInbetween, showIndividual, benchmarkRuns):
+def benchmarkMap(root, canvas, showInbetween, border, showIndividual, benchmarkRuns):
     if not benchmarkRuns.isdigit():
         print("ERROR - must provide benchmark runs as integer")
         return
@@ -115,38 +114,41 @@ def benchmarkMap(root, canvas, showInbetween, showIndividual, benchmarkRuns):
     now = datetime.now()
     totalRuntime = datetime.now()
     for i in range(int(benchmarkRuns)):
-        totalRuntime += resetMap()
+        totalRuntime += resetMap(border)
         if showInbetween:
             updateCanvas(root, canvas)
-        totalRuntime += collapseMap(root, canvas, showInbetween)
+        totalRuntime += collapseMap(root, canvas, showInbetween, border)
         if showIndividual:
             updateCanvas(root, canvas)
     print(f"benchmarking finished - {totalRuntime - now}")
 
-def benchmarkButtonAction(root, canvas, showInbetween, showIndividual, benchmarkRuns):
-    benchmarkMap(root, canvas, showInbetween, showIndividual, benchmarkRuns)
+def benchmarkButtonAction(root, canvas, showInbetween, border, showIndividual, benchmarkRuns):
+    benchmarkMap(root, canvas, showInbetween, border, showIndividual, benchmarkRuns)
     updateCanvas(root, canvas)
 
-def collapseButtonAction(root, canvas, showInbetween):
+def collapseButtonAction(root, canvas, showInbetween, border):
     updateCanvas(root, canvas)
-    resetMap()
+    resetMap(border)
     updateCanvas(root, canvas)
-    collapseMap(root, canvas, showInbetween)
+    collapseMap(root, canvas, showInbetween, border)
     updateCanvas(root, canvas)
 
 def buildInterface(frame, root, canvas):
     showInbetweenVar = tk.IntVar(value=1)
     showInbetweenCb = tk.Checkbutton(frame, text="Show inbetween", variable=showInbetweenVar)
     showInbetweenCb.pack()
-    collapseButton = tk.Button(frame, text="Collapse", width=20, command=lambda: collapseButtonAction(root, canvas, showInbetweenVar.get()))
+    borderVar = tk.IntVar(value=1)
+    borderCb = tk.Checkbutton(frame, text="Border", variable=borderVar)
+    borderCb.pack()
+    collapseButton = tk.Button(frame, text="Collapse", width=20, command=lambda: collapseButtonAction(root, canvas, showInbetweenVar.get(), borderVar.get()))
     collapseButton.pack()
-    benchmarkRunsVar = tk.StringVar(value="5")
+    benchmarkRunsVar = tk.StringVar(value="1")
     benchmarkRunsEntry = tk.Entry(frame, textvariable=benchmarkRunsVar)
     benchmarkRunsEntry.pack()
     showIndividualVar = tk.IntVar(value=1)
     showIndividualCb = tk.Checkbutton(frame, text="Show individual", variable=showIndividualVar)
     showIndividualCb.pack()
-    benchmarkButton = tk.Button(frame, text=f"Benchmark", width=20, command=lambda: benchmarkButtonAction(root, canvas, showInbetweenVar.get(), showIndividualVar.get(), benchmarkRunsVar.get()))
+    benchmarkButton = tk.Button(frame, text=f"Benchmark", width=20, command=lambda: benchmarkButtonAction(root, canvas, showInbetweenVar.get(), borderVar.get(), showIndividualVar.get(), benchmarkRunsVar.get()))
     benchmarkButton.pack()
 
 def updateCanvasTile(canvas, i, j):
